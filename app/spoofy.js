495d0e96d472fc48226a7e6827958c47db562cba
VincentKempers <vincent.m.kempers@gmail.com>
2018-03-20 12:11:00 +0100

    added oba app and did the npm init

---
+++ app/spoofy.js
@@ -0,0 +1,122 @@
+var data = [
+  {
+    "title":"10 jaar Ivy Green.",
+    "embed": "spotify:album:4PDP7EKx9YiZcrNcmNAis6"
+  },
+  {
+    "title":"Green on Red.",
+    "embed": "spotify:album:3beOzSOb3URSWWiWBis707"
+  },
+  {
+    "title":"Herman Brood & his Wild Romance.",
+    "embed":"spotify:album:0iLppOEBKBgPfAOGBbTF9Q"
+  },
+  {
+    "title":"Chris Rea.",
+    "embed": "spotify:album:0rUMR4B7I2718LhLR3z0qj"
+  },
+  {
+    "title":"SO!",
+    "embed": "spotify:user:1230268882:playlist:0FvQb8WRmhT0S8fJXhbKHU"
+  },
+  {
+    "title":"Barrence Whitfield and the Savages.",
+    "embed": "spotify:album:7BkGkTLvqQra0FWN3YtLF0"
+  },
+  {
+    "title":"Made in Holland.",
+    "embed": "spotify:album:2T0MfqCHRB885nf92CKemA"
+  },
+  {
+    "title":"Anne Clark + Band",
+    "embed" : "spotify:album:15eoAwjHark9zAPE5z2GKJ"
+  },
+  {
+    "title":"Pep Club.",
+    "embed" : "spotify:album:10yei2eL0tlBVJokSZ4s5p"
+  },
+  {
+    "title":"Steel Pulse.",
+    "embed": "spotify:album:3BVtphNjEG5djS7JKPhn7T"
+  },
+  {
+    "title":"Lenny Kravitz.",
+    "embed": "spotify:album:70Vuh3jYUMO8LLP5BaqZMb"
+  },
+  {
+    "title":"Lydia Lunch.",
+    "embed":"spotify:album:2rDp5iFby86SSjNtpm3TjN"
+  },
+  {
+    "title":"Siobhan.",
+    "embed":"spotify:album:6OrKDX3ok7cwA9OGpgOQzO"
+  },
+  {
+    "title":"Made in Holland.",
+    "embed": "spotify:album:2T0MfqCHRB885nf92CKemA"
+  },
+  {
+    "title":"Red Hot Chili Peppers.",
+    "embed": "spotify:album:1oOkcBu5bgkUzZTvKD1m8z"
+  },
+  {
+    "title":"Ivy Green.",
+    "embed": "spotify:album:4PDP7EKx9YiZcrNcmNAis6"
+  },
+  {
+    "title":"Ramones.",
+    "embed": "spotify:album:0YaEQO8yfc9NHEDiZu8ssX"
+  },
+  {
+    "title":"Ultra's.",
+    "embed": "spotify:album:3bBAAh56UpqMmwuq9j4ZcN"
+  },
+  {
+    "title":"PepClub.",
+    "embed" : "spotify:album:10yei2eL0tlBVJokSZ4s5p"
+  },
+  {
+    "title":"Made in Holland.",
+    "embed": "spotify:album:2T0MfqCHRB885nf92CKemA"
+  },
+  {
+    "title":"Ziggy Marley & Melody Makers.",
+    "embed": "spotify:album:5qPo9GTYNkMFfe40oUL7VZ"
+  },
+  {
+    "title":"Solomon Burke.",
+    "embed": "spotify:album:1PcxRhtVoFMXAkojPK1GWN"
+  },
+  {
+    "title":"Boogie Down Productions.",
+    "embed":"spotify:album:4Vy8Ddvc1tlQV3sYs7nWuD"
+  },
+  {
+    "title":"Love/Hate.",
+    "embed": "spotify:album:7ySvIgQt4DmewXYtuR8EYk"
+  },
+  {
+    "title":"Mark Stewart & The Maffia.",
+    "embed":"spotify:album:5Tt3XfakaB1M1VbpubCBR1"
+  },
+  {
+    "title":"Disco party met Prince Charles.",
+    "embed":"spotify:album:1upoyWoLrf9FdG7VWB0akK"
+  },
+  {
+    "title":"Made in Holland.",
+    "embed": "spotify:album:2T0MfqCHRB885nf92CKemA"
+  },
+  {
+    "title":"Festival for Jimi Hendrix.",
+    "embed": "spotify:user:spotify:playlist:37i9dQZF1DWTNV753no4ic"
+  },
+  {
+    "title":"Snowy White.",
+    "embed":"spotify:album:6LshkqHsxlW480jyAleAD4"
+  },
+  {
+    "title":"John Lennon herdenkingsdag.",
+    "embed": "spotify:user:spotify:playlist:37i9dQZF1DX3WXnClxyI5d" 
+  }
+]
