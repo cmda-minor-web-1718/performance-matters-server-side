3a814d65174257786f37db54461aab967cd7fbba
Rick712 <rick.buter@gmail.com>
2018-03-22 14:24:22 +0100

    i have no idea what i'm doing

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const ejs = require('ejs')
 const express = require('express')
+const fetch = require('node-fetch')
+
 const app = express()
+
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-const port = 3000
+const port = 3000,
+    host = 'https://www.pokeapi.co/api/v2/pokemon/?limit=151'
 
-app.get('/', function(req, res){
-    res.render('index')
+fetch( 'https://www.pokeapi.co/api/v2/pokemon/?limit=151' )
+    .then(res => res.json())
+    .then(json => console.log(json))
+
+
+app.get('/', function(json){
+    // console.log(res)
+    ejs.render('index.ejs', {pokemon: json})
 })
 
 app.listen(port, function(){
