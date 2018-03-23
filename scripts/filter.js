92a60a21eee50610012589d67d697dc673295315
VincentKempers <vincent.m.kempers@gmail.com>
2018-03-23 12:10:01 +0100

    edited the javascript a bit

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,9 +1,12 @@
-function myFunction() {
+
+
+function filterFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
     ul = document.getElementById("myUL");
     li = ul.getElementsByTagName("li");
+
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
