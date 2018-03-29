d62d5ed3216cc16db7cf1057751a3df405f509c8
Rick712 <rick.buter@gmail.com>
2018-03-29 15:05:51 +0200

    Service worker is working, i guess

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,9 +1,37 @@
-
-
-
-
-self.addEventListener('install', event => event.waitUntil(
-    caches.open('pokemon')
-        .then(cache => cache.add('../views/offline/offline.html'))
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('pokeCache')
+        .then(function(cache) {
+            cache.addAll([
+                '../',
+                '../views',
+                'script/script.js',
+                'styles/style.css'
+            ])
+        })
         .then(self.skipWaiting())
-));
+    )
+})
+
+self.addEventListener('fetch', function(event){
+
+    event.respondWith(caches.match(event.request)
+        .then(function(response){
+            if(response) {
+                return response // Geef het aan mij als het er al is
+            } 
+            else{
+                return fetch(event.request)
+                
+                .then(function(newResponse){
+                    return caches.open('dynamicPokeCache') // Of voeg het hier aan toe als het er nog niet is
+                    
+                    .then(function(cache) {
+                        cache.put(event.request.url, newResponse.clone())
+                        return newResponse
+                    })
+                })
+            }
+        })
+    )
+})
